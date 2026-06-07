a5ba8694813e410523dabf0a8851dd24fc9b001f
thanktriet <thanktriet@users.noreply.github.com>
2026-06-07 09:44:36 +0700

    feat: thêm preload bridge cho Electron IPC

---
+++ electron/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
+  onUpdateAvailable: (cb) => ipcRenderer.on('update-available', (_e, info) => cb(info)),
+  onUpdateDownloaded: (cb) => ipcRenderer.on('update-downloaded', (_e, info) => cb(info)),
+  installUpdate: () => ipcRenderer.send('install-update'),
+  onUpdateProgress: (cb) => ipcRenderer.on('update-progress', (_e, progress) => cb(progress)),
+});
